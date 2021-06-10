df31e3cf9d70eea52b959bb62d9c097a61679eea
Omar587 <63128085+Omar587@users.noreply.github.com>
2021-06-10 14:34:05 -0600

    Update blockChain.c

--- blockChain.c
+++ blockChain.c
@@ -1,9 +1,5 @@
 /*-------------------------------------------------------
 # Student's Name: Orest Dushkevich, Axel Nguyen, and Omar Mohamed
-# Assignment #3 Milestone 3
-# Lab Section: X02L and X01l
-# CMPUT 201
-# Instructor's Name: Nesrine Abbas
 *------------------------------------------------------*/
 
 #include "blockChain.h"
